1b68221511e6a5ac0b5ba642cfc86b63afc66d93
lengocthuan <lengocthuan2581997@gmail.com>
2019-01-16 15:54:24 +0700

    html css can ban

---
+++ resources/views/demo01.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<title>Internship K1 2019</title>
+<link href="greenglobal.ico" type="image/x-icon" rel="icon">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('css/demo01.css') }}" >
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="wrapper">
+  <header class="header-container">
+    <div class="container">
+        <div class="row">
+          <div class="logo" id="logo">
+            <a href="/" class="logo-link">
+              <img src="img/greenglobal.jpg" alt="logo" class="img-logo">
+            </a>
+          </div>
+          <nav class="nav-menu">
+            <ul class="list-menu">
+              <li class="menu-item">
+                <a class="item-link" href="#">Home</a>                
+              </li>
+              <li class="menu-item">
+                <a class="item-link" href="#">About us</a>
+              </li>
+              <li class="menu-item">
+                <a class="item-link" href="#">Products</a>
+              </li>
+              <li class="menu-item">
+                <a class="item-link" href="#">Download</a>
+              </li>
+              <li class="menu-item has-sub-menu">
+                <a class="item-link" href="#">My account</a>
+                <ul class="sub-menu">
+                  <li class="menu-item">
+                    <a class="item-link" href="#">My profile</a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="item-link" href="#">Logout</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+    </div>
+  </header>
+</div>
+<script src="js/app.js"></script>
+</body>
+</html>
